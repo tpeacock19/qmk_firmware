2dbbe7e5e43e01af6d7097851dd5800026dbab47
Trey Peacock <git@treypeacock.com>
2020-11-21 15:15:20 -0700

    version 1
    Colemak base, QWERTY backup, Fn & Numberpad layers

--- keyboards/fc660c/keymaps/tpeacock19/config.h
+++ keyboards/fc660c/keymaps/tpeacock19/config.h
@@ -17,3 +17,35 @@
 #pragma once
 
 // place overrides here
+
+/*****************************************************************************/
+/*                             Auto Shift Timeout                            */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/*					Temporary Rebind for setting timeout
+Key Name 	Description
+KC_ASDN 	Lower the Auto Shift timeout variable (down)
+KC_ASUP 	Raise the Auto Shift timeout variable (up)
+KC_ASRP 	Report your current Auto Shift timeout value
+KC_ASON 	Turns on the Auto Shift Function
+KC_ASOFF 	Turns off the Auto Shift Function
+KC_ASTG 	Toggles the state of the Auto Shift feature 					 */
+/*****************************************************************************/
+
+/* #define AUTO_SHIFT_TIMEOUT 165 */
+
+/* Do not Auto Shift special keys, which include -_, =+, [{, ]}, ;:, ‘“, ,<, .>, and /? */
+/* #define NO_AUTO_SHIFT_SPECIAL */
+
+/* Do not Auto Shift numeric keys, [0-9] */
+/* #define NO_AUTO_SHIFT_NUMERIC */
+
+/* Do not Auto Shift alpha characters, [a-Z] */
+/* #define NO_AUTO_SHIFT_ALPHA */
+
+/*****************************************************************************/
+/*                             Oneshot Settings                              */
+/*****************************************************************************/
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
--- keyboards/fc660c/keymaps/tpeacock19/keymap.c
+++ keyboards/fc660c/keymaps/tpeacock19/keymap.c
@@ -16,43 +16,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-enum tpeacock19_layers { _QWERTY, _COLEMAK, _FNM, _NUM};
+enum tpeacock19_layers { _COLEMAK, _QWERTY, _FNM, _NUM};
 
-enum tpeacock19_keycodes { QWERTY = SAFE_RANGE, COLEMAK };
+enum tpeacock19_keycodes { COLEMAK = SAFE_RANGE, QWERTY };
+
+#define GRAVE_MODS  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT)|MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)|MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
 
 #define FNM MO(_FNM)
-
-// Shift / paren setup
-#ifndef LSPO_KEYS
-#    define LSPO_KEYS KC_LSFT, LSPO_MOD, LSPO_KEY
-#endif
-#ifndef RSPC_KEYS
-#    define RSPC_KEYS KC_RSFT, RSPC_MOD, RSPC_KEY
-#endif
-
+#define NUM TG(_NUM)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* BASE layer: Default Layer
-   * ,--------------------------------------------------------------------------------------------------.
-   * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | Backspace |  | `   |
-   * |-----------------------------------------------------------------------------------------+  +-----+
-   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  Bksl  |  | Del |
-   * |-----------------------------------------------------------------------------------------+  +-----+
-   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
-   * |--------------------------------------------------------------------------------------------+
-   * | Shift      |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift       | Up  |
-   * +--------------------------------------------------------------------------------------------+-----+
-   * | Ctrl  | Alt   | Gui  |               Space                | Fn  | Ctrl | Alt   | Left| Down|Right|
-   * `--------------------------------------------------------------------------------------------------´
-   */
-  [_QWERTY] = LAYOUT(
-        KC_GESC, KC_1,   KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,  KC_0,   KC_MINS,KC_EQL, KC_BSPC,    KC_GRV,
-        KC_TAB,  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,  KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,    KC_DEL,
-        KC_CAPS , KC_A,   KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,  KC_SCLN,KC_QUOT,     KC_ENT,
-        KC_LSFT, KC_Z,   KC_X,   KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,KC_DOT,KC_SLSH,KC_RSFT,     KC_UP,
-        KC_LCTL, KC_LALT,KC_LGUI,          KC_SPC,                      FNM,KC_RCTL,KC_RALT,    KC_LEFT,KC_DOWN,KC_RGHT
-                     ),
-    /* Colemak Layer
+    /* Default Colemak Layer
    * ,--------------------------------------------------------------------------------------------------.
    * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | Backspace |  | `   |
    * |-----------------------------------------------------------------------------------------+  +-----+
@@ -62,57 +36,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------------------------------+
    * | Shift      |  X  |  C  |  D  |  V  |  Z  |  K  |  H  |  ,  |  .  |  /  | Shift       | Up  |
    * +--------------------------------------------------------------------------------------------+-----+
-   * | Caps  | Alt   | Gui  |               Space                | Fn  | Ctrl | Alt   | Left| Down|Right|
+   * | Caps  | Gui   | Alt  |               Space                | Fn  | Ctrl | Alt   | Left| Down|Right|
    * `--------------------------------------------------------------------------------------------------´
    */
   [_COLEMAK] = LAYOUT(
-        KC_GESC, KC_1,   KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,  KC_0,   KC_MINS,KC_EQL, KC_BSPC,    KC_GRV,
-        KC_TAB,  KC_Q,   KC_W,   KC_F,  KC_P,  KC_B,  KC_J,  KC_L,  KC_U,   KC_Y,  KC_SCLN,KC_LBRC,KC_RBRC,KC_BSLS,    KC_DEL,
-        CTL_T(KC_ESC), KC_A,   KC_R,   KC_S,  KC_T,  KC_G,  KC_M,  KC_N,  KC_E,   KC_I,  KC_O   ,KC_QUOT,     KC_ENT,
-        LSFT_T(), KC_X,   KC_C,   KC_D,  KC_V,  KC_Z,  KC_K,  KC_H,  KC_COMM,KC_DOT,KC_SLSH,KC_RSFT,     KC_UP,
-        KC_CAPS, KC_LALT,KC_LGUI,          KC_SPC,                      FNM,KC_RCTL,KC_RALT,    KC_LEFT,KC_DOWN,KC_RGHT
+        KC_GESC,        KC_1,   KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_GRV,
+        KC_TAB,         KC_Q,   KC_W,   KC_F,  KC_P,  KC_B,  KC_J,  KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,  KC_DEL,
+        CTL_T(KC_ESC),  KC_A,   KC_R,   KC_S,  KC_T,  KC_G,  KC_M,  KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT,          KC_ENT,
+        OSM(MOD_LSFT),  KC_X,   KC_C,   KC_D,  KC_V,  KC_Z,  KC_K,  KC_H,  KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT),    KC_UP,
+        KC_CAPS, KC_LGUI, KC_LALT,                    KC_SPC,                       FNM,    KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
                       ),
+    /* QWERTY Layer
+   * ,--------------------------------------------------------------------------------------------------.
+   * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | Backspace |  | `   |
+   * |-----------------------------------------------------------------------------------------+  +-----+
+   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  Bksl  |  | Del |
+   * |-----------------------------------------------------------------------------------------+  +-----+
+   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * |--------------------------------------------------------------------------------------------+
+   * | Shift      |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift       | Up  |
+   * +--------------------------------------------------------------------------------------------+-----+
+   * | Ctrl  | Gui   | Alt  |               Space                | Fn  | Ctrl | Alt   | Left| Down|Right|
+   * `--------------------------------------------------------------------------------------------------´
+   */
+  [_QWERTY] = LAYOUT(
+        KC_GESC, KC_1,   KC_2,   KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_GRV,
+        KC_TAB,  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_DEL,
+        KC_CAPS, KC_A,   KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT, KC_Z,   KC_X,   KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,          KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,             KC_SPC,                       FNM,    KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+                     ),
    /* FN layer
    * ,--------------------------------------------------------------------------------------------------.
    * | `   | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |       Mute|  | Vol-|
    * |-----------------------------------------------------------------------------------------+  +-----+
-   * |        |     |     |     |     |     |     |     |PrtSc| Slck| Paus|     |     |        |  | Vol+|
+   * |        |     | Prev | TGL | Next |     |     |     |PrtSc| Slck|     |     |     |      |  | Vol+|
    * |-----------------------------------------------------------------------------------------+  +-----+
-   * |         |     |     |     |     |     |     |     | Home| PgUp|     |     |             |
+   * |         |     |     |QWRTY|COLMK|     |     |     | Home| PgUp|     |     |             |
    * |--------------------------------------------------------------------------------------------+
    * |            |     |     |     |     |     |     |     | End | PgDn|     | Mouse Btn 1 | MsU |
    * +--------------------------------------------------------------------------------------------+-----+
-   * |       | Reset |      |                                    |      |      |      | MsL | MsD | MsR |
+   * |       | Reset |      |                 NUM                |      |      |      | MsL | MsD | MsR |
    * `--------------------------------------------------------------------------------------------------´
    */
   [_FNM] = LAYOUT(
         KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_MUTE,     KC_VOLU,
-        _______,_______,_______,_______,_______,_______,_______,_______,KC_PSCR,KC_SLCK,KC_PAUS,_______,_______,  RESET,     KC_VOLD,
-        _______,_______,_______, QWERTY,COLEMAK,_______,_______,_______,KC_HOME,KC_PGUP,_______,_______,     _______,
+        _______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,KC_PSCR,KC_SLCK,_______,_______,_______,_______,     KC_VOLD,
+        _______,_______,_______, QWERTY,COLEMAK,_______,_______,_______,KC_HOME,KC_PGUP,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,KC_END, KC_PGDN,_______,KC_BTN1,     KC_MS_U,
-        _______,_______,_______,                _______,               _______,_______,_______,       KC_MS_L,KC_MS_D,KC_MS_R
+        _______,  RESET,_______,                NUM,                    _______,_______,_______,     KC_MS_L,KC_MS_D,KC_MS_R
                   ),
-
-   /* FN layer
+   /* NUM layer
    * ,--------------------------------------------------------------------------------------------------.
    * | `   | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |       Mute|  | Vol-|
    * |-----------------------------------------------------------------------------------------+  +-----+
-   * |        |     |     |     |     |     |     |     |PrtSc| Slck| Paus|     |     |        |  | Vol+|
+   * |        |     |     |     |     |     |     |  1  |  2  |  3  |  -  |     |     |        |  | Vol+|
    * |-----------------------------------------------------------------------------------------+  +-----+
-   * |         |     |     |     |     |     |     |     | Home| PgUp|     |     |             |
+   * |   NUM   |     |     |QWRTY|COLMK|     |  0  |  4  |  5  |  6  |  +  |  *  |             |
    * |--------------------------------------------------------------------------------------------+
-   * |            |     |     |     |     |     |     |     | End | PgDn|     | Mouse Btn 1 | MsU |
+   * |            |     |     |     |     |     |     |  7  |  8  |  9  |  /  | Mouse Btn 1 | MsU |
    * +--------------------------------------------------------------------------------------------+-----+
    * |       | Reset |      |                                    |      |      |      | MsL | MsD | MsR |
    * `--------------------------------------------------------------------------------------------------´
    */
   [_NUM] = LAYOUT(
         KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_MUTE,     KC_VOLU,
-        _______,_______,_______,_______,_______,_______,_______,_______,KC_PSCR,KC_SLCK,KC_PAUS,_______,_______,  RESET,     KC_VOLD,
-        _______,_______,_______, QWERTY,COLEMAK,_______,_______,_______,KC_HOME,KC_PGUP,_______,_______,     _______,
-        _______,_______,_______,_______,_______,_______,_______,_______,KC_END, KC_PGDN,_______,KC_BTN1,     KC_MS_U,
-        _______,_______,_______,                _______,               _______,_______,_______,       KC_MS_L,KC_MS_D,KC_MS_R
+        _______,_______,_______,_______,_______,_______,_______,KC_1,KC_2,KC_3,KC_KP_MINUS,_______,_______,_______, KC_VOLD,
+            NUM,_______,_______, QWERTY,COLEMAK,_______,KC_0,KC_4,KC_5,KC_6,KC_KP_PLUS,KC_KP_ASTERISK,_______,
+        _______,_______,_______,_______,_______,_______,_______,KC_7,KC_8,KC_9,KC_KP_SLASH,KC_BTN1,     KC_MS_U,
+        _______,  RESET,_______,            _______,                   _______,_______,_______,          KC_MS_L,KC_MS_D,KC_MS_R
     )
+};
+
+/* This is a list of user defined functions. F(N) corresponds to item N
+   of this list.
+ */
+const uint16_t PROGMEM fn_actions[] = {
+  [0] = ACTION_FUNCTION(0),  // Calls action_function()
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
