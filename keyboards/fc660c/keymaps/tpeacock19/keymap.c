809d0b6b75a264a087b7dfa8a469f6b0ede7af72
Trey Peacock <git@treypeacock.com>
2020-12-02 13:39:15 -0700

    version 2 & pyenv

--- keyboards/fc660c/keymaps/tpeacock19/keymap.c
+++ keyboards/fc660c/keymaps/tpeacock19/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------------------------------+
    * | Shift      |  X  |  C  |  D  |  V  |  Z  |  K  |  H  |  ,  |  .  |  /  | Shift       | Up  |
    * +--------------------------------------------------------------------------------------------+-----+
-   * | Caps  | Gui   | Alt  |               Space                | Fn  | Ctrl | Alt   | Left| Down|Right|
+   * | Caps  | Gui   | Alt  |               Space                | Alt | Ctrl | Fn    | Left| Down|Right|
    * `--------------------------------------------------------------------------------------------------´
    */
   [_COLEMAK] = LAYOUT(
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,   KC_W,   KC_F,  KC_P,  KC_B,  KC_J,  KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,  KC_DEL,
         CTL_T(KC_ESC),  KC_A,   KC_R,   KC_S,  KC_T,  KC_G,  KC_M,  KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT,          KC_ENT,
         OSM(MOD_LSFT),  KC_X,   KC_C,   KC_D,  KC_V,  KC_Z,  KC_K,  KC_H,  KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT),    KC_UP,
-        KC_CAPS, KC_LGUI, KC_LALT,                    KC_SPC,                       FNM,    KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_CAPS, KC_LGUI, KC_LALT,                    KC_SPC,                       KC_RALT,KC_RCTL,     FNM, KC_LEFT, KC_DOWN, KC_RGHT
                       ),
     /* QWERTY Layer
    * ,--------------------------------------------------------------------------------------------------.
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------------------------------+
    * | Shift      |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift       | Up  |
    * +--------------------------------------------------------------------------------------------+-----+
-   * | Ctrl  | Gui   | Alt  |               Space                | Fn  | Ctrl | Alt   | Left| Down|Right|
+   * | Ctrl  | Gui   | Alt  |               Space                | Alt | Ctrl |  Fn   | Left| Down|Right|
    * `--------------------------------------------------------------------------------------------------´
    */
   [_QWERTY] = LAYOUT(
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_DEL,
         KC_CAPS, KC_A,   KC_S,   KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, KC_Z,   KC_X,   KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,          KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,             KC_SPC,                       FNM,    KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,             KC_SPC,                       KC_RALT,KC_RCTL,     FNM, KC_LEFT, KC_DOWN, KC_RGHT
                      ),
    /* FN layer
    * ,--------------------------------------------------------------------------------------------------.
@@ -88,23 +88,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   ),
    /* NUM layer
    * ,--------------------------------------------------------------------------------------------------.
-   * | `   | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |       Mute|  | Vol-|
+   * | `   | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |           |  | Vol-|
    * |-----------------------------------------------------------------------------------------+  +-----+
-   * |        |     |     |     |     |     |     |  1  |  2  |  3  |  -  |     |     |        |  | Vol+|
+   * |        |     |     |     |     |     |     |  7  |  8  |  9  |  -  |     |     |        |  | Vol+|
    * |-----------------------------------------------------------------------------------------+  +-----+
-   * |   NUM   |     |     |QWRTY|COLMK|     |  0  |  4  |  5  |  6  |  +  |  *  |             |
+   * |   NUM   |     |     |QWRTY|COLMK|     |     |  4  |  5  |  6  |  +  |  *  |             |
    * |--------------------------------------------------------------------------------------------+
-   * |            |     |     |     |     |     |     |  7  |  8  |  9  |  /  | Mouse Btn 1 | MsU |
+   * |            |     |     |     |     |     |     |  1  |  2  |  3  |  /  | Mouse Btn 1 | MsU |
    * +--------------------------------------------------------------------------------------------+-----+
-   * |       | Reset |      |                                    |      |      |      | MsL | MsD | MsR |
+   * |       | Reset |      |                0                   |      |      |      | MsL | MsD | MsR |
    * `--------------------------------------------------------------------------------------------------´
    */
   [_NUM] = LAYOUT(
-        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_MUTE,     KC_VOLU,
-        _______,_______,_______,_______,_______,_______,_______,KC_1,KC_2,KC_3,KC_KP_MINUS,_______,_______,_______, KC_VOLD,
+        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,_______,     KC_VOLU,
+        _______,_______,_______,_______,_______,_______,_______,KC_7,KC_8,KC_9,KC_KP_MINUS,_______,_______,_______, KC_VOLD,
             NUM,_______,_______, QWERTY,COLEMAK,_______,KC_0,KC_4,KC_5,KC_6,KC_KP_PLUS,KC_KP_ASTERISK,_______,
-        _______,_______,_______,_______,_______,_______,_______,KC_7,KC_8,KC_9,KC_KP_SLASH,KC_BTN1,     KC_MS_U,
-        _______,  RESET,_______,            _______,                   _______,_______,_______,          KC_MS_L,KC_MS_D,KC_MS_R
+        _______,_______,_______,_______,_______,_______,_______,KC_1,KC_2,KC_3,KC_KP_SLASH,KC_BTN1,     KC_MS_U,
+        _______,  RESET,_______,            KC_0,             _______,_______,_______,          KC_MS_L,KC_MS_D,KC_MS_R
     )
 };
 
